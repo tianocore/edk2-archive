fee743e9f184c23d60a65e19be95c3b6c19c330e
jwang36 <jwang36@6f19259b-4bc3-4df7-8a09-765794883524>
2007-03-14 13:05:06 +0000

    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/edk2@2462 6f19259b-4bc3-4df7-8a09-765794883524

--- edksetup.bat
+++ edksetup.bat
@@ -9,129 +9,125 @@
 @REM WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 @REM
 
-@REM set following environment in this file or in command shell
-
-@REM usage: edksetup.bat [Rebuild] [ForceRebuild] [Reconfig]
-@REM if the argument, skip is present, only the paths and the
-@REM test and set of environment settings are performed. 
-
-@REM ##############################################################
-@REM # You should not have to modify anything below this line
-@REM #
-
 @echo off
 
-@REM
-@REM Check the required system environment variables
-@REM
+REM ##############################################################
+REM # You should not have to modify anything below this line
+REM #
+
+if /I "%1"=="-h" goto usage
+if /I "%1"=="-help" goto usage
+if /I "%1"=="--help" goto usage
+if /I "%1"=="/h" goto usage
+if /I "%1"=="/?" goto usage
+if /I "%1"=="/help" goto usage
+
+if NOT "%1"=="" set EDK_TOOLS_PATH=%~f1
+
+REM
+REM Check the required system environment variables
+REM
+
+echo ###########################################################
+echo  Setup workspace environments
+echo ###########################################################
 
 :check_vc
-if defined VCINSTALLDIR goto check_cygwin
+if defined VCINSTALLDIR goto setup_workspace
 if defined VS71COMNTOOLS (
   call "%VS71COMNTOOLS%\vsvars32.bat"
 ) else (
   echo.
-  echo !!! WARNING !!!! Cannot find Visual Studio !!!
-  echo.
-)
-
-:check_cygwin
-if defined CYGWIN_HOME goto setup_workspace
-if exist c:\cygwin (
-  set CYGWIN_HOME=c:\cygwin
-) else (
-  echo.
-  echo !!! WARNING !!!! Not set CYGWIN_HOME, gcc build may not be used !!!
+  echo !!!WARNING!!! Cannot find Visual Studio !!!
   echo.
 )
 
 :setup_workspace
-@REM
-@REM Set the WORKSPACE to the current working directory
-@REM
+REM
+REM Set the WORKSPACE to the current working directory
+REM
 set WORKSPACE=%CD%
 
-@REM
-@REM check the EDK_TOOLS_PATH
-@REM
+REM
+REM check the EDK_TOOLS_PATH
+REM
 if not defined EDK_TOOLS_PATH goto no_tools_path
-if not exist %EDK_TOOLS_PATH% (
-  echo.
-  echo !!! WARNING !!!! %EDK_TOOLS_PATH% doesn't exist. Please check EDK_TOOLS_PATH !!!
-  echo.
-  goto end
-)
+if exist %EDK_TOOLS_PATH% goto set_path
+echo.
+echo !!!WARNING!!! %EDK_TOOLS_PATH% doesn't exist. %WORKSPACE%\Tools will be used !!!
+echo.
 
 :no_tools_path
 if exist %WORKSPACE%\Tools (
   set EDK_TOOLS_PATH=%WORKSPACE%\Tools
 ) else (
   echo.
-  echo !!! WARNING !!!! No tools path found. Please set EDK_TOOLS_PATH !!!
+  echo !!!WARNING!!! No tools path found. Please set EDK_TOOLS_PATH !!!
   echo.
   goto end
 )
 
+:set_path
 if defined WORKSPACE_TOOLS_PATH goto check_path
-set PATH=%EDK_TOOLS_PATH%\Bin
+set PATH=%EDK_TOOLS_PATH%\Bin;%EDK_TOOLS_PATH%\Bin\Win32;%PATH%
+set WORKSPACE_TOOLS_PATH=%EDK_TOOLS_PATH%
 echo Setting the PATH variable to include the EDK_TOOLS_PATH for this WORKSPACE
 goto path_ok
 
 :check_path
 if "%EDK_TOOLS_PATH%"=="%WORKSPACE_TOOLS_PATH%" goto path_ok
-set PATH=%EDK_TOOLS_PATH%;%PATH%
+set PATH=%EDK_TOOLS_PATH%\Bin;%EDK_TOOLS_PATH%\Bin\Win32;%PATH%
 set WORKSPACE_TOOLS_PATH=%EDK_TOOLS_PATH%
 echo Resetting the PATH variable to include the EDK_TOOLS_PATH for this WORKSPACE
 
 :path_ok
-
-@if /I "%1"=="-h" goto Usage
-@if /I "%1"=="-help" goto Usage
-@if /I "%1"=="--help" goto Usage
-@if /I "%1"=="/h" goto Usage
-@if /I "%1"=="/?" goto Usage
-@if /I "%1"=="/help" goto Usage
-@if /I "%1"=="ForceRebuild" goto ForceBuild
-@if /I "%1"=="Reconfig" goto Reconfig
-@if /I "%1"=="Rebuild" goto NormalBuild
-@if NOT "%1"=="" goto Usage
-
-:NormalBuild
-@REM
-@REM Start to build the Framework Tools
-@REM
-
+echo           PATH = %PATH%
 echo.
-echo Building the Framework Tools
+echo      WORKSPACE = %WORKSPACE%
+echo EDK_TOOLS_PATH = %EDK_TOOLS_PATH%
 echo.
 
-call %EDK_TOOLS_PATH%\setup.bat
-@REM
-@REM Done!!!
-@REM
+REM
+REM copy *.template to %EDK_TOOLS_PATH%\Conf
+REM
+if NOT exist %EDK_TOOLS_PATH%\Conf mkdir %EDK_TOOLS_PATH%\Conf
+if NOT exist %EDK_TOOLS_PATH%\Conf\FrameworkDatabase.db (
+  echo copying ... FrameworkDatabase.template to %EDK_TOOLS_PATH%\Conf\FrameworkDatabase.db
+  copy %WORKSPACE%\Conf\FrameworkDatabase.template %EDK_TOOLS_PATH%\Conf\FrameworkDatabase.db > nul
+)
+if NOT exist %EDK_TOOLS_PATH%\Conf\target.txt (
+  echo copying ... target.template to %EDK_TOOLS_PATH%\Conf\target.txt
+  copy %WORKSPACE%\Conf\target.template %EDK_TOOLS_PATH%\Conf\target.txt > nul
+)
+if NOT exist %EDK_TOOLS_PATH%\Conf\tools_def.txt (
+  echo copying ... tools_def.template to %EDK_TOOLS_PATH%\Conf\tools_def.txt
+  copy %WORKSPACE%\Conf\tools_def.template %EDK_TOOLS_PATH%\Conf\tools_def.txt > nul
+)
+
+REM
+REM copy XMLSchema to %EDK_TOOLS_PATH%\Conf\XMLSchema
+REM
+REM echo copying ... XMLSchema to %EDK_TOOLS_PATH%\Conf\XMLSchema
+xcopy %WORKSPACE%\Conf\XMLSchema %EDK_TOOLS_PATH%\Conf\XMLSchema /S /I /D /F /Q > nul
+
+:tool_setup
+echo.
+echo ###########################################################
+echo  Setup Framework Tools
+echo ###########################################################
+echo.
+
+call %EDK_TOOLS_PATH%\tool_setup.bat
+REM
+REM Done!!!
+REM
 goto end
 
-
-:ForceBuild 
-call %EDK_TOOLS_PATH%\setup.bat cleanall
-
-:Reconfig
-@REM
-@REM Reinstall all config files
-@REM
-call %EDK_TOOLS_PATH%\setup.bat Reconfig
-goto end
-
-:Usage
+:usage
 echo.
-echo  Usage: %0 [Rebuild] [ForceRebuild] [Reconfig]
-echo         Rebuild:       Incremental build, only build those updated tools; 
-echo         ForceRebuild:  Rebuild all tools neither updated or not; 
-echo         Reconfig:      Reinstall target.txt, tools_def.txt, FrameworkDatabase.db. 
+echo  "Usage: %0 [/? | /h | /help | -h | -help | --help] [tools_path]"
 echo.
-echo  Note that target.template, tools_def.template, FrameworkDatabase.template will be
-echo  only copied to target.txt, tools_def.txt, FrameworkDatabase.db respectively if they
-echo  are not existed. Using option [Reconfig] to do the force copy. 
+echo                      tools_path       Tools' path. EDK_TOOLS_PATH will be set to this path.
 echo.
 
 :end
