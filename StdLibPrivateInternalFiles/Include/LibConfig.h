992d172fadb02126872e46291a86141f7f8c371e
lpleahy <lpleahy@6f19259b-4bc3-4df7-8a09-765794883524>
2011-08-08 22:39:08 +0000

    Bring up to EDK2 trunk revision 12080
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/EADK@12104 6f19259b-4bc3-4df7-8a09-765794883524

--- StdLibPrivateInternalFiles/Include/LibConfig.h
+++ StdLibPrivateInternalFiles/Include/LibConfig.h
@@ -8,7 +8,7 @@
   these libraries have been tested with.  Changing this configuration may
   produce code that will not build or may not run.  Change at your own risk.
 
-  Copyright (c) 2010, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2010 - 2011, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials are licensed and made available under
   the terms and conditions of the BSD License that accompanies this distribution.
   The full text of the license may be found at
