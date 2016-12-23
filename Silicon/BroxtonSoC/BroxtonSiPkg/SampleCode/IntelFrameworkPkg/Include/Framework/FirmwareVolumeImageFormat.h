797b371ee7d5b4531038c8cb4328eb793a0a12dd
Guo Mang <mang.guo@intel.com>
2016-12-23 13:09:36 +0800

    BroxtonSiPkg: Add SampleCode/IntelFrameworkPkg
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Guo Mang <mang.guo@intel.com>

---
+++ Silicon/BroxtonSoC/BroxtonSiPkg/SampleCode/IntelFrameworkPkg/Include/Framework/FirmwareVolumeImageFormat.h
@@ -0,0 +1,37 @@
+/** @file
+  This file defines the data structures that are architecturally defined for file
+  images loaded via the FirmwareVolume protocol.  The Firmware Volume specification
+  is the basis for these definitions.
+
+  Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php.
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __FIRMWARE_VOLUME_IMAGE_FORMAT_H__
+#define __FIRMWARE_VOLUME_IMAGE_FORMAT_H__
+
+//
+// Bit values for AuthenticationStatus
+//
+#define EFI_AGGREGATE_AUTH_STATUS_PLATFORM_OVERRIDE 0x000001
+#define EFI_AGGREGATE_AUTH_STATUS_IMAGE_SIGNED      0x000002
+#define EFI_AGGREGATE_AUTH_STATUS_NOT_TESTED        0x000004
+#define EFI_AGGREGATE_AUTH_STATUS_TEST_FAILED       0x000008
+#define EFI_AGGREGATE_AUTH_STATUS_ALL               0x00000f
+
+#define EFI_LOCAL_AUTH_STATUS_PLATFORM_OVERRIDE     0x010000
+#define EFI_LOCAL_AUTH_STATUS_IMAGE_SIGNED          0x020000
+#define EFI_LOCAL_AUTH_STATUS_NOT_TESTED            0x040000
+#define EFI_LOCAL_AUTH_STATUS_TEST_FAILED           0x080000
+#define EFI_LOCAL_AUTH_STATUS_ALL                   0x0f0000
+
+#endif
+
