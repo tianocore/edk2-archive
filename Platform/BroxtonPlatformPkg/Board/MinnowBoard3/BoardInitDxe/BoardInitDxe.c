63b4452feae23b01db9996f3e547a5300fb884a3
Guo Mang <mang.guo@intel.com>
2016-12-23 13:28:42 +0800

    BroxtonPlatformPkg: Add MinnowBoard3/BoardInitDxe
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Guo Mang <mang.guo@intel.com>

---
+++ Platform/BroxtonPlatformPkg/Board/MinnowBoard3/BoardInitDxe/BoardInitDxe.c
@@ -0,0 +1,68 @@
+/** @file
+  Board specific functions in DXE phase to be set as dynamic PCD and consumed by
+  commmon platform code.
+
+  Copyright (c) 2009 - 2016, Intel Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php.
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include "BoardInitDxe.h"
+
+GET_BOARD_NAME mMb3GetBoardNamePtr = Mb3GetBoardName;
+
+CHAR16*
+EFIAPI
+Mb3GetBoardName (
+  IN  UINT8                   BoardId
+  )
+{
+  STATIC CHAR16  BoardName[40];
+
+  DEBUG ((EFI_D_INFO,  "BoardInitDxe: GetBoardName - Minnow Board v3\n"));
+
+  UnicodeSPrint (BoardName, sizeof (BoardName), L"Minnow Board v3 (%02x)", BoardId);
+
+  if (BoardId != (UINT8) BOARD_ID_MINNOW) {
+    return NULL;
+  } else {
+    return BoardName;
+  }
+}
+
+
+/**
+  Set PCDs for board specific functions.
+
+  @param[in]  ImageHandle   ImageHandle of the loaded driver.
+  @param[in]  SystemTable   Pointer to the EFI System Table.
+
+  @retval     EFI_SUCCESS   The handlers were registered successfully.
+
+**/
+EFI_STATUS
+EFIAPI
+Mb3BoardInitDxeConstructor (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  UINT8       BoardId;
+
+  BoardId = PcdGet8 (PcdBoardId);
+  if (BoardId != (UINT8) BOARD_ID_MINNOW) {
+    return EFI_SUCCESS;
+  }
+
+  PcdSet64 (PcdGetBoardNameFunc, (UINT64) mMb3GetBoardNamePtr);
+
+  return EFI_SUCCESS;
+}
+
