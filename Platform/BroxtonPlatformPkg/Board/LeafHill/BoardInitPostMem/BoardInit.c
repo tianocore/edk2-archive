b0223dc831c7a8d46e7cbaece3f46a2de437fd39
Guo Mang <mang.guo@intel.com>
2016-12-23 13:27:34 +0800

    BroxtonPlatformPkg: Add LeafHill/BoardInitPostMem
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Guo Mang <mang.guo@intel.com>

---
+++ Platform/BroxtonPlatformPkg/Board/LeafHill/BoardInitPostMem/BoardInit.c
@@ -0,0 +1,122 @@
+/** @file
+  Board Init driver.
+
+  Copyright (c) 2010 - 2016, Intel Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php.
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <PiPei.h>
+#include <Library/PeiServicesLib.h>
+#include <Library/PcdLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Guid/PlatformInfo_Aplk.h>
+#include <Ppi/BoardInitSignalling.h>
+#include "BoardInit.h"
+#include "BoardInitMiscs.h"
+
+EFI_STATUS
+EFIAPI
+LeafHillPostMemInitCallback (
+  IN CONST EFI_PEI_SERVICES     **PeiServices,
+  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
+  IN VOID                       *Ppi
+  );
+
+
+static EFI_PEI_NOTIFY_DESCRIPTOR mLeafHillPostMemNotifyList = {
+  (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
+  &gBoardPostMemInitStartGuid,
+  LeafHillPostMemInitCallback
+};
+
+static EFI_PEI_PPI_DESCRIPTOR mLeafHillPostMemDonePpi = {
+  (EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
+  &gBoardPostMemInitDoneGuid,
+  NULL
+};
+
+EFI_STATUS
+EFIAPI
+LeafHillPostMemInitCallback (
+  IN CONST EFI_PEI_SERVICES     **PeiServices,
+  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
+  IN VOID                       *Ppi
+  )
+{
+  EFI_STATUS                       Status;
+  VOID                             *Instance;
+  UINT8                            BoardId;
+  UINT8                            FabId;
+
+  Status = PeiServicesLocatePpi (
+             &gBoardPostMemInitDoneGuid,
+             0,
+             NULL,
+             &Instance
+             );
+  if (!EFI_ERROR (Status)) {
+    DEBUG ((EFI_D_INFO,  "Leaf Hill Post Mem Init callback: Skip\n"));
+    return EFI_SUCCESS;
+  }
+
+  BoardId = (UINT8) PcdGet8 (PcdBoardId);
+  FabId = (UINT8) PcdGet8 (PcdFabId);
+  DEBUG ((EFI_D_INFO,  "PostMemInit: BoardId == 0x%X, FabId == 0x%X\n", BoardId, FabId));
+  if (BoardId != (UINT8) BOARD_ID_LFH_CRB) {
+    DEBUG ((EFI_D_INFO,  "LeafHill Post Mem Init callback: Skip\n"));
+    return EFI_SUCCESS;
+  }
+
+  DEBUG ((EFI_D_INFO,  "LeafHill Post Mem Init callback\n"));
+
+  //
+  // Set init function PCD
+  //
+  PcdSet64 (PcdBoardPostMemInitFunc, (UINT64) (UINTN) MultiPlatformInfoInit);
+
+  //
+  // Add init steps here
+  //
+  //
+  // Install a flag signalling a board's post mem init is done
+  //
+  Status = PeiServicesInstallPpi (&mLeafHillPostMemDonePpi);
+
+  return EFI_SUCCESS;
+}
+
+
+/**
+  This function performs Board initialization in Pre-Memory.
+
+  @retval     EFI_SUCCESS           The PPI is installed and initialized.
+  @retval     EFI ERRORS            The PPI is not successfully installed.
+  @retval     EFI_OUT_OF_RESOURCES  No enough resoruces (such as out of memory).
+
+**/
+EFI_STATUS
+EFIAPI
+LeafHillInitConstructor (
+  IN       EFI_PEI_FILE_HANDLE  FileHandle,
+  IN CONST EFI_PEI_SERVICES     **PeiServices
+  )
+{
+  EFI_STATUS                        Status;
+
+  DEBUG ((EFI_D_INFO,  "LeafHill Post Mem Init Constructor \n"));
+
+  DEBUG ((EFI_D_INFO,  "Notify on Post Mem Init Start PPI \n"));
+  Status = PeiServicesNotifyPpi (&mLeafHillPostMemNotifyList);
+
+  return Status;
+}
+
---
+++ Platform/BroxtonPlatformPkg/Board/LeafHill/BoardInitPostMem/BoardInitMiscs.h
@@ -0,0 +1,126 @@
+/** @file
+  Multiplatform initialization header file.
+  This file includes package header files, library classes.
+
+  Copyright (c) 2010 - 2016, Intel Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php.
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _MULTIPLATFORM_LIB_H_
+#define _MULTIPLATFORM_LIB_H_
+
+#define LEN_64M       0x4000000
+//
+// Default PCI32 resource size
+//
+#define RES_MEM32_MIN_LEN   0x38000000
+
+#define RES_IO_BASE   0x0D00
+#define RES_IO_LIMIT  0xFFFF
+#include <PiDxe.h>
+#include <Library/BaseLib.h>
+#include <FrameworkPei.h>
+#include "PlatformBaseAddresses.h"
+#include "ScAccess.h"
+#include "SetupMode.h"
+#include "PlatformBootMode.h"
+#include "CpuRegs.h"
+#include "Platform.h"
+#include "CMOSMap.h"
+#include <Ppi/Stall.h>
+#include <Guid/SetupVariable.h>
+#include <Ppi/AtaController.h>
+#include <Ppi/BootInRecoveryMode.h>
+#include <Ppi/ReadOnlyVariable2.h>
+#include <Ppi/Capsule.h>
+#include <Guid/EfiVpdData.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/PcdLib.h>
+#include <Library/IoLib.h>
+#include <Library/HobLib.h>
+#include <Library/BaseLib.h>
+#include <Library/SteppingLib.h>
+#include <IndustryStandard/Pci22.h>
+#include <Guid/FirmwareFileSystem.h>
+#include <Guid/MemoryTypeInformation.h>
+#include <Ppi/Reset.h>
+#include <Ppi/EndOfPeiPhase.h>
+#include <Ppi/MemoryDiscovered.h>
+#include <Guid/GlobalVariable.h>
+#include <Ppi/RecoveryModule.h>
+#include <Ppi/DeviceRecoveryModule.h>
+#include <Guid/Capsule.h>
+#include <Guid/RecoveryDevice.h>
+#include <Ppi/MasterBootMode.h>
+#include <Guid/PlatformInfo_Aplk.h>
+
+#define EFI_ACPI_OEM_ID_DEFAULT    SIGNATURE_64('I', 'N', 'T', 'E', 'L', ' ', ' ', ' ')     // max 6 chars
+#define EFI_ACPI_OEM_ID1           SIGNATURE_64('I', 'N', 'T', 'E', 'L', '1', ' ', ' ')     // max 6 chars
+#define EFI_ACPI_OEM_ID2           SIGNATURE_64('I', 'N', 'T', 'E', 'L', '2', ' ', ' ')     // max 6 chars
+
+#define EFI_ACPI_OEM_TABLE_ID_DEFAULT   SIGNATURE_64('E', 'D', 'K', '2', ' ', ' ', ' ', ' ')
+#define EFI_ACPI_OEM_TABLE_ID1          SIGNATURE_64('E', 'D', 'K', '2', '_', '1', ' ', ' ')
+#define EFI_ACPI_OEM_TABLE_ID2          SIGNATURE_64('E', 'D', 'K', '2', '_', '2', ' ', ' ')
+
+//
+// Default Vendor ID and Subsystem ID
+//
+#define SUBSYSTEM_VENDOR_ID1   0x8086
+#define SUBSYSTEM_DEVICE_ID1   0x1999
+#define SUBSYSTEM_SVID_SSID1   (SUBSYSTEM_VENDOR_ID1 + (SUBSYSTEM_DEVICE_ID1 << 16))
+
+#define SUBSYSTEM_VENDOR_ID2   0x8086
+#define SUBSYSTEM_DEVICE_ID2   0x1888
+#define SUBSYSTEM_SVID_SSID2   (SUBSYSTEM_VENDOR_ID2 + (SUBSYSTEM_DEVICE_ID2 << 16))
+
+#define SUBSYSTEM_VENDOR_ID   0x8086
+#define SUBSYSTEM_DEVICE_ID   0x1234
+#define SUBSYSTEM_SVID_SSID   (SUBSYSTEM_VENDOR_ID + (SUBSYSTEM_DEVICE_ID << 16))
+
+EFI_STATUS
+GetPlatformInfoHob (
+  IN CONST EFI_PEI_SERVICES     **PeiServices,
+  OUT EFI_PLATFORM_INFO_HOB     **PlatformInfoHob
+  );
+
+EFI_STATUS
+MultiPlatformGpioTableInit (
+  IN CONST EFI_PEI_SERVICES     **PeiServices,
+  IN EFI_PLATFORM_INFO_HOB      *PlatformInfoHob
+  );
+
+EFI_STATUS
+MultiPlatformGpioProgram (
+  IN CONST EFI_PEI_SERVICES     **PeiServices,
+  IN EFI_PLATFORM_INFO_HOB      *PlatformInfoHob
+  );
+
+EFI_STATUS
+MultiPlatformInfoInit (
+  IN CONST EFI_PEI_SERVICES     **PeiServices,
+  IN OUT EFI_PLATFORM_INFO_HOB  *PlatformInfoHob
+  );
+
+EFI_STATUS
+InitializeBoardOemId (
+  IN CONST EFI_PEI_SERVICES      **PeiServices,
+  IN EFI_PLATFORM_INFO_HOB        *PlatformInfoHob
+  );
+
+EFI_STATUS
+InitializeBoardSsidSvid (
+  IN CONST EFI_PEI_SERVICES       **PeiServices,
+  IN EFI_PLATFORM_INFO_HOB        *PlatformInfoHob
+  );
+
+#endif
+
